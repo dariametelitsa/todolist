9957eb28daa7fd32e2fa3210e8a46eaa32a7b22b
dariametelitsa <dariametelitsa@gmail.com>
2024-03-26 14:06:31 +0300

    added tasks component

---
+++ src/components/tasks/Tasks.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import Todolist from "../todolist/Todolist";
+import { FilterValuesType, TasksPropsType, TaskType } from "../../data/dataPropsTypes";
+import { data1 } from "../../data/Data";
+import styles from './Tasks.module.scss';
+
+export const Tasks: React.FC<TasksPropsType> = (props: TasksPropsType) => {
+    let {title, tasks, students} = props;
+
+    let [tasksFiltered, setTasks] = useState<Array<TaskType>>(data1.tasks);
+
+    let [filter, setFilter] = useState<FilterValuesType>('all')
+    function removeTask(id: number) {
+        let filteredTasks = tasks.filter((t) => t.id !== id);
+        setTasks(filteredTasks);
+    }
+
+    function changeFilter(value: FilterValuesType) {
+        setFilter(value);
+    }
+
+    let tasksForTodoList = tasksFiltered;
+
+    if (filter === 'completed') {
+        tasksForTodoList = tasks.filter(t => t.isDone)
+    }
+
+    if (filter === 'active') {
+        tasksForTodoList = tasks.filter(t => !t.isDone)
+    }
+
+
+    return (
+        <div>
+            <Todolist title={title} tasks={tasksForTodoList} removeTask={removeTask} changeFilter={changeFilter} />
+            <ul>
+                {
+                    students.map(s => {
+                        return (
+                            <li className={styles.listItem}>{s}</li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+    );
+};
