9633820c7e135e102d33d94be391c53657761093
dariametelitsa <dariametelitsa@gmail.com>
2024-03-27 19:01:28 +0300

    added test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App, { sum } from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('sum should be correct', () => {
+  let a = 2;
+  let b = 3;
+  let c = 5;
+
+  const result = sum(a,b);
+
+  expect(result).toBe(5);
+})
--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,11 @@ import './App.scss';
 import { Tasks } from "./components/tasks/Tasks";
 import { data1, data2 } from "./data/Data";
 
+export const sum = (a: number, b: number): number => {
+    return a + b;
+}
 
 function App() {
-
-    // let [tasks, setTasks] = useState<Array<TaskType>>([
-    //     {id: 1, title: "CSS&HTML", isDone: true},
-    //     {id: 2, title: "JS", isDone: true},
-    //     {id: 3, title: "React", isDone: false},
-    //     {id: 4, title: "Redux", isDone: false},
-    // ]);
-    //
-    // let [filter, setFilter] = useState<FilterValuesType>('all')
-    //
-    // function removeTask(id: number) {
-    //     let filteredTasks = tasks.filter((t) => t.id !== id);
-    //     console.log('here');
-    //     setTasks(filteredTasks);
-    // }
-    //
-    // function changeFilter(value: FilterValuesType) {
-    //     setFilter(value);
-    // }
-    //
-    // let tasksForTodoList = tasks;
-    //
-    // if (filter === 'completed') {
-    //     tasksForTodoList = tasks.filter(t => t.isDone)
-    // }
-    //
-    // if (filter === 'active') {
-    //     tasksForTodoList = tasks.filter(t => !t.isDone)
-    // }
-
 
     return (
         <div className="App">
@@ -44,7 +17,6 @@ function App() {
             {/*    removeTask={removeTask}*/}
             {/*    changeFilter={changeFilter}*/}
             {/*/>*/}
-
             <Tasks title={data1.title} tasks={data1.tasks} students={data1.students}/>
             <Tasks title={data2.title} tasks={data1.tasks} students={data2.students}/>
         </div>
