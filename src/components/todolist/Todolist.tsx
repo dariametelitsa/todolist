48446c1baa94ff337b2faf95a8746eeefded8a17
dariametelitsa <dariametelitsa@gmail.com>
2024-04-01 17:26:28 +0300

    added task add behavior (code style)

--- src/components/todolist/Todolist.tsx
+++ src/components/todolist/Todolist.tsx
@@ -3,47 +3,66 @@ import { TodolistPropsType } from "../../data/dataPropsTypes";
 import { Button } from "../button/Button";
 import { Input } from "../input/Input";
 
-const  Todolist:React.FC<TodolistPropsType> = ({title, tasks, removeTask, changeFilter, id, addTask}: TodolistPropsType)  => {
-	let [task, setTask] = useState('');
+const Todolist: React.FC<TodolistPropsType> = ({
+                                                   title,
+                                                   tasks,
+                                                   removeTask,
+                                                   changeFilter,
+                                                   id,
+                                                   addTask
+                                               }: TodolistPropsType) => {
+    let [task, setTask] = useState('');
 
-	const onClickButtonHandler = () => {
-		addTask(id, task);
-		setTask('');
-	}
-	return (
-		<div className={'todolist'}>
-			<h3>{title}</h3>
-			<div className={'addTask'}>
-				<Input changeTitle={setTask} title={task}/>
-				<Button name={'Add'} callBack={onClickButtonHandler}></Button>
-			</div>
-			<ul>
-				{
-					tasks.length === 0 ? (
-						<p>Задач нет</p>
-					) : (
-						tasks.map((task, index) => {
-							return (
-								<li key={index}> <input type={"checkbox"} checked={task.isDone}/>
-									<span>{task.title}</span>
+    const onClickButtonHandler = () => {
+        addTask(id, task);
+        setTask('');
+    }
 
-									<Button
-										name={'x'}
-										callBack={() => {removeTask(task.id)}}
-									/>
-								</li>
-							)})
-						)
-				}
-			</ul>
-			<div className={'tabs'}>
-				<button onClick={() => {
-					changeFilter('all')}}>All</button>
-				<button onClick={() => {changeFilter('active')}}>Active</button>
-				<button onClick={() => {changeFilter('completed')}}>Completed</button>
-			</div>
-		</div>
-	);
+    return (
+        <div className={'todolist'}>
+            <h3>{title}</h3>
+            <div className={'addTask'}>
+                <Input changeTitle={setTask} title={task}/>
+                <Button name={'Add'} callBack={onClickButtonHandler}></Button>
+            </div>
+            <ul>
+                {
+                    tasks.length === 0 ? (
+                        <p>Задач нет</p>
+                    ) : (
+                        tasks.map((task, index) => {
+                            return (
+                                <li key={index}><input type={"checkbox"} checked={task.isDone}/>
+                                    <span>{task.title}</span>
+
+                                    <Button
+                                        name={'x'}
+                                        callBack={() => {
+                                            removeTask(task.id)
+                                        }}
+                                    />
+                                </li>
+                            )
+                        })
+                    )
+                }
+            </ul>
+            <div className={'tabs'}>
+                <button onClick={() => {
+                    changeFilter('all')
+                }}>All
+                </button>
+                <button onClick={() => {
+                    changeFilter('active')
+                }}>Active
+                </button>
+                <button onClick={() => {
+                    changeFilter('completed')
+                }}>Completed
+                </button>
+            </div>
+        </div>
+    );
 }
 
 
