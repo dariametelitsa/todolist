44c7d28aa58a30efd5882ff388bbd05025ed6132
dariametelitsa <dariametelitsa@gmail.com>
2024-05-06 13:13:26 +0300

    added max input length and empty mode

--- src/components/editableSpan/EditableSpan.tsx
+++ src/components/editableSpan/EditableSpan.tsx
@@ -1,23 +1,24 @@
-// @flow 
+// @flow
 import * as React from 'react';
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useState } from 'react';
 
 
 export type EditableSpanProps = {
     oldTitle: string
     idToChange: string
     updateItem: (id: string, newTitle: string) => void
+    maxLength?: number
 };
 
-export const EditableSpan = ({idToChange, oldTitle, updateItem}: EditableSpanProps) => {
+export const EditableSpan = ({idToChange, oldTitle, updateItem, maxLength = 25}: EditableSpanProps) => {
 
     const [editMode, setEditMode] = useState(false)
     const [newTitle, setNewTitle] = useState<string>(oldTitle);
 
     const activateEditModeHandler = () => {
         setEditMode(!editMode);
-        if(editMode) {
-            updateItem(idToChange, newTitle);
+        if (editMode) {
+            newTitle ? updateItem(idToChange, newTitle) : updateItem(idToChange, oldTitle);
         }
     }
 
@@ -29,11 +30,18 @@ export const EditableSpan = ({idToChange, oldTitle, updateItem}: EditableSpanPro
         if (e.key === 'Escape') {
             setEditMode(!editMode);
         }
+        if (e.key === 'Enter') {
+            activateEditModeHandler();
+        }
     }
 
     return (
         editMode
-            ? <input value={newTitle} onChange={onChangeTitleHandler} onKeyDown={onKeyDownHandler} onBlur={activateEditModeHandler} autoFocus/>
+            ? <input value={newTitle}
+                     onChange={onChangeTitleHandler}
+                     onKeyDown={onKeyDownHandler}
+                     onBlur={activateEditModeHandler}
+                     maxLength={maxLength} autoFocus/>
             : <span onDoubleClick={activateEditModeHandler}>{oldTitle}</span>
     );
 };
