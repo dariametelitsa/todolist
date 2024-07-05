1ebc1edaeb7ecb685c3c3525d6299adeda495aa8
Daria Metelitsa <dariametelitsa@gmail.com>
2024-07-05 11:51:19 +0300

    changed styles for tasks is done

--- src/features/todolistList/todolist/Todolist.styles.ts
+++ src/features/todolistList/todolist/Todolist.styles.ts
@@ -1,13 +1,15 @@
 import { SxProps } from '@mui/material'
 
 export const filterButtonsContainerSx: SxProps = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    mt: 1
+  display: 'flex',
+  justifyContent: 'space-between',
+  mt: 1,
 }
 
-export const getListItemSx = (isDone: boolean | undefined) :SxProps => ({
-    p: 0,
-    justifyContent: 'space-between',
-    opacity: isDone ? 0.5 : 1
+export const getListItemSx = (isDone: boolean | undefined): SxProps => ({
+  p: 0,
+  justifyContent: 'space-between',
+  // opacity: isDone ? 0.5 : 1,
+  textDecoration: isDone ? 'line-through' : 'none',
+  color: (theme) => (isDone ? 'grey' : 'inherit'),
 })
