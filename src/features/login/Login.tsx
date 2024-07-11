10199ac8518cb499ca943ba226ea8658e7764b6a
Daria Metelitsa <dariametelitsa@gmail.com>
2024-07-11 20:17:43 +0300

    added formik for login

--- src/features/login/Login.tsx
+++ src/features/login/Login.tsx
@@ -7,32 +7,93 @@ import FormGroup from '@mui/material/FormGroup'
 import FormLabel from '@mui/material/FormLabel'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
+import { useFormik } from 'formik'
+
+type ErrorsType = {
+  email?: string
+  password?: string
+}
 
 export const Login = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      rememberMe: false,
+    },
+    validate: (values) => {
+      const errors: ErrorsType = {}
+      if (!values.email) {
+        errors.email = 'Required email'
+      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address'
+      }
+      if (!values.password) {
+        errors.password = 'Required pass'
+      }
+      return errors
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2))
+      //alert(JSON.stringify(values))
+    },
+  })
+  //console.log(formik.values)
+  //console.log(formik.touched)
+
   return (
     <Grid container justifyContent={'center'}>
       <Grid item justifyContent={'center'}>
-        <FormControl>
-          <FormLabel>
-            <p>
-              To log in get registered{' '}
-              <a href={'https://social-network.samuraijs.com/'} target={'_blank'}>
-                here
-              </a>
-            </p>
-            <p>or use common test account credentials:</p>
-            <p>Email: free@samuraijs.com</p>
-            <p>Password: free</p>
-          </FormLabel>
-          <FormGroup>
-            <TextField label="Email" margin="normal" />
-            <TextField type="password" label="Password" margin="normal" />
-            <FormControlLabel label={'Remember me'} control={<Checkbox />} />
-            <Button type={'submit'} variant={'contained'} color={'primary'}>
-              Login
-            </Button>
-          </FormGroup>
-        </FormControl>
+        <form onSubmit={formik.handleSubmit}>
+          <FormControl>
+            <FormLabel>
+              {/*<p>*/}
+              {/*  To log in get registered{' '}*/}
+              {/*  <a href={'https://social-network.samuraijs.com/'} target={'_blank'}>*/}
+              {/*    here*/}
+              {/*  </a>*/}
+              {/*</p>*/}
+              {/*<p>or use common test account credentials:</p>*/}
+              <p>Email: free@samuraijs.com</p>
+              <p>Password: free</p>
+            </FormLabel>
+            <FormGroup>
+              <TextField
+                label="Email"
+                margin="normal"
+                name="email"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
+              />
+              {formik.touched.email && formik.errors.email && (
+                <div style={{ color: 'tomato' }}>{formik.errors.email}</div>
+              )}
+              <TextField
+                type="password"
+                label="Password"
+                margin="normal"
+                name="password"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.password}
+              />
+              {formik.touched.password && formik.errors.password && (
+                <div style={{ color: 'tomato' }}>{formik.errors.password}</div>
+              )}
+              <FormControlLabel
+                label={'Remember me'}
+                control={<Checkbox />}
+                onChange={formik.handleChange}
+                checked={formik.values.rememberMe}
+                name="rememberMe"
+              />
+              <Button type={'submit'} variant={'contained'} color={'primary'}>
+                Login
+              </Button>
+            </FormGroup>
+          </FormControl>
+        </form>
       </Grid>
     </Grid>
   )
