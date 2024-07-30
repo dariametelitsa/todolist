c758e7ab78c7121b1f6e22b51f8bad6912abedef
Daria Metelitsa <dariametelitsa@gmail.com>
2024-07-30 16:01:14 +0300

    refactored instance

--- src/common/instance/instance.ts
+++ src/common/instance/instance.ts
@@ -4,6 +4,6 @@ export const instance = axios.create({
   baseURL: 'https://social-network.samuraijs.com/api/1.1',
   withCredentials: true,
   headers: {
-    'API-KEY': '1c0d9a02-17ae-40c8-8a16-b7733f0e908d',
+    'API-KEY': process.env.REACT_APP_API_KEY,
   },
 });
