f825f82ec2d311eace27d20fc71500a957a7d594
Daria Metelitsa <dariametelitsa@gmail.com>
2024-08-07 17:31:19 +0300

    added suspense fot todolists

--- src/features/todolistList/TodolistlistsContainer.tsx
+++ src/features/todolistList/TodolistlistsContainer.tsx
@@ -1,13 +1,16 @@
-import React, { useCallback } from 'react';
+import React, { lazy, Suspense, useCallback } from 'react';
 import Grid from '@mui/material/Unstable_Grid2';
-import Todolist from './todolist/Todolist';
 import { Navigate } from 'react-router-dom';
-import { PATH } from '../../common/routes/PATH';
-import { useAppDispatch } from '../../app/store';
+import { PATH } from 'common/routes/PATH';
+import { useAppDispatch } from 'app/store';
 import { useSelector } from 'react-redux';
 import { selectIsLoggedIn } from '../auth/model/authSlice';
 import { addTodolist, selectTodolists } from './model/todolistsSlice';
-import { AddItem } from '../../common/components/addItem/AddItem';
+import { AddItem } from 'common/components/addItem/AddItem';
+import CircularProgress from '@mui/material/CircularProgress';
+// import Todolist from './todolist/Todolist';
+
+const Todolist = lazy(() => import('./todolist/Todolist'));
 
 export const TodolistlistsContainer: React.FC = () => {
   const todoLists = useSelector(selectTodolists);
@@ -29,12 +32,13 @@ export const TodolistlistsContainer: React.FC = () => {
       <Grid container spacing={2} sx={{ m: 0, mb: 5 }}>
         <AddItem addItem={addTodolistCallback}></AddItem>
       </Grid>
-
-      <Grid container spacing={3}>
-        {todoLists.map((td) => {
-          return <Todolist todolist={td} key={td.id} />;
-        })}
-      </Grid>
+      <Suspense fallback={<CircularProgress />}>
+        <Grid container spacing={3}>
+          {todoLists.map((td) => {
+            return <Todolist todolist={td} key={td.id} />;
+          })}
+        </Grid>
+      </Suspense>
     </>
   );
 };
