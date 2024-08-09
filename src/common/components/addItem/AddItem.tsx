6144096855eff9eee89270395815a1c8511df2b5
Daria Metelitsa <dariametelitsa@gmail.com>
2024-08-09 19:04:08 +0300

    refactored assItem functions names

--- src/common/components/addItem/AddItem.tsx
+++ src/common/components/addItem/AddItem.tsx
@@ -1,24 +1,24 @@
-import * as React from 'react'
-import s from './AddItem.module.scss'
-import TextField from '@mui/material/TextField'
-import IconButton from '@mui/material/IconButton'
-import AddBoxIcon from '@mui/icons-material/AddBox'
-import { useItemForm } from './hooks/useAddItem'
+import * as React from 'react';
+import s from './AddItem.module.scss';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import { useItemForm } from 'common/components/addItem/lib/useAddItem';
 
 export type AddItemProps = {
-  addItem: (name: string) => void
-  disabled?: boolean
-}
+  addItem: (name: string) => void;
+  disabled?: boolean;
+};
 
 export const AddItem = React.memo(({ addItem, disabled }: AddItemProps) => {
-  const { itemTitle, onChangeInputHandler, onKeyDownHandler, itemInputError, onClickAddItemHandler } =
-    useItemForm(addItem)
+  const { itemTitle, changeInputHandler, saveItemTitleHandler, itemInputError, onClickAddItemHandler } =
+    useItemForm(addItem);
 
   return (
     <div className={s.addItemBox}>
       <TextField
-        onChange={onChangeInputHandler}
-        onKeyDown={onKeyDownHandler}
+        onChange={changeInputHandler}
+        onKeyDown={saveItemTitleHandler}
         label={'Введите заголовок'}
         variant="outlined"
         value={itemTitle}
@@ -32,5 +32,5 @@ export const AddItem = React.memo(({ addItem, disabled }: AddItemProps) => {
         <AddBoxIcon />
       </IconButton>
     </div>
-  )
-})
+  );
+});
--- src/common/components/addItem/lib/useAddItem.ts
+++ src/common/components/addItem/lib/useAddItem.ts
@@ -1,51 +1,51 @@
-import { ChangeEvent, useState } from 'react'
-import * as React from 'react'
+import { ChangeEvent, useState } from 'react';
+import * as React from 'react';
 
 export const useItemForm = (addItem: (name: string) => void) => {
   //local state - not business tasks
-  let [itemTitle, setNewItemTitle] = useState('')
-  let [itemInputError, setItemInputError] = useState<string | null>(null)
+  let [itemTitle, setNewItemTitle] = useState('');
+  let [itemInputError, setItemInputError] = useState<string | null>(null);
 
-  const isTitleToLong = itemTitle.length > 30
-  const ifTaskCanAdded = itemTitle && !isTitleToLong
+  const isTitleToLong = itemTitle.length > 30;
+  const ifTaskCanAdded = itemTitle && !isTitleToLong;
 
   const onClickAddItemHandler = () => {
-    addItemWithCheck()
-  }
+    addItemWithCheck();
+  };
 
-  const onKeyDownHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+  const saveItemTitleHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
-      addItemWithCheck()
+      addItemWithCheck();
     }
-  }
+  };
   const addItemWithCheck = () => {
-    const trimmedTaskTitle = itemTitle.trim()
+    const trimmedTaskTitle = itemTitle.trim();
     if (ifTaskCanAdded) {
       if (trimmedTaskTitle) {
-        addItem(trimmedTaskTitle)
-        setNewItemTitle('')
+        addItem(trimmedTaskTitle);
+        setNewItemTitle('');
       } else {
-        setItemInputError('Title is required')
-        setNewItemTitle('')
+        setItemInputError('Title is required');
+        setNewItemTitle('');
       }
     }
-  }
+  };
 
-  const onChangeSetItemTitle = (title: string) => {
-    setNewItemTitle(title)
-    setItemInputError(null)
-  }
+  const setItemTitleHandler = (title: string) => {
+    setNewItemTitle(title);
+    setItemInputError(null);
+  };
 
-  const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    onChangeSetItemTitle(event.currentTarget.value)
-    isTitleToLong ? setItemInputError('Too long') : setItemInputError(null)
-  }
+  const changeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setItemTitleHandler(event.currentTarget.value);
+    isTitleToLong ? setItemInputError('Too long') : setItemInputError(null);
+  };
 
   return {
     itemTitle,
-    onChangeInputHandler,
-    onKeyDownHandler,
+    changeInputHandler,
+    saveItemTitleHandler,
     itemInputError,
     onClickAddItemHandler,
-  }
-}
+  };
+};
