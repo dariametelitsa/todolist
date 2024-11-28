b807aacf2de4f8f0c345ded5c5480fd323f4eea6
Daria Metelitsa <dariametelitsa@gmail.com>
2024-11-28 11:33:26 +0300

    feat: add instance settings

--- src/common/instance/instance.ts
+++ src/common/instance/instance.ts
@@ -5,5 +5,6 @@ export const instance = axios.create({
   withCredentials: true,
   headers: {
     'API-KEY': process.env.REACT_APP_API_KEY,
+    Authorization: `Bearer ${process.env.REACT_APP_AUTH_TOKEN}`,
   },
 });
